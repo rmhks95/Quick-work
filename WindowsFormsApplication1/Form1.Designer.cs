60ec7c5eee90fa833be4f563580071a0e5c52785
ryanhuse <ryanhuse@L-ZBOOK-H>
2017-04-21 14:32:46 -0500

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start_but = new System.Windows.Forms.Button();
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start_but
+            // 
+            this.Start_but.Location = new System.Drawing.Point(53, 63);
+            this.Start_but.Name = "Start_but";
+            this.Start_but.Size = new System.Drawing.Size(75, 23);
+            this.Start_but.TabIndex = 0;
+            this.Start_but.Text = "Start";
+            this.Start_but.UseVisualStyleBackColor = true;
+            this.Start_but.Click += new System.EventHandler(this.Start_but_Click);
+            // 
+            // PathBox
+            // 
+            this.PathBox.Location = new System.Drawing.Point(53, 26);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.Size = new System.Drawing.Size(129, 20);
+            this.PathBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 98);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PathBox);
+            this.Controls.Add(this.Start_but);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_but;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_but_Click(object sender, EventArgs e)
+        {
+
+            string path = PathBox.Text;
+
+            int total = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path)).Count();
+            string[] upload = new string[total];
+            string line;
+
+            StreamReader SR = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path));
+            for (int i = 0; i < total; i++)
+            {
+                line = SR.ReadLine();
+                if(line != null)
+                    upload[i] = line;
+                
+            }
+            SR.Close();
+
+            var open = File.Create(path);
+            open.Close();
+            using (var sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path), true))
+            {
+                for (int i = total-1; i > -1; i--)
+                {
+                        sw.WriteLine(upload[i]);
+
+                }
+                sw.Close();
+            }
+
+        }
+    }
+}
